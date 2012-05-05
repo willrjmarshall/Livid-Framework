acc7796c4264a3614421b7d3bf5a58be57aba961
Will Marshall <will@willmarshall.me>
2012-05-05 18:56:08 +1000

    Refresh state on power cycle of device w00t

---
+++ LividControlSurface.py
@@ -0,0 +1,41 @@
+import Live
+from _Framework.ControlSurface import ControlSurface
+
+class LividControlSurface(ControlSurface):
+  """ Custom control surface with boilerplate handled """
+
+  def __init__(self, c_instance):
+    ControlSurface.__init__(self, c_instance)
+    self.awaiting_refresh = False
+    self._device_selection_follows_track_selection = True
+    self._suggested_input_port = ('Ohm')
+    self._suggested_output_port = ('Ohm')
+    self.set_suppress_rebuild_requests(True) #Turn rebuild back on, now that we're done setting up
+    self.setup_mixer()
+    self.setup_session()
+    self.setup_transport()
+    self.set_suppress_rebuild_requests(False) #Turn rebuild back on, now that we're done setting up
+    self.log_message("Finished setting up")
+
+  def refresh_state(self):
+    ControlSurface.refresh_state(self)
+    if not self.awaiting_refresh:
+      self.awaiting_refresh = True
+      self.schedule_message(1, self.refresh_on_timer)
+
+  def refresh_on_timer(self):
+    self.awaiting_refresh = False
+    ControlSurface.refresh_state(self)
+    self.log_message("Refreshed state after 1 second delay")
+
+
+
+  def setup_mixer(self):
+    raise AssertionError, 'Function setup_mixer must be overridden by subclass'
+
+  def setup_session(self):
+    raise assertionerror, 'function setup_session must be overridden by subclass'
+
+  def setup_transport(self):
+    raise assertionerror, 'function setup_transport must be overridden by subclass'
+
