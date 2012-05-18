42c2231074324115bde9bc6622f61b44383815a7
Will Marshall <will@willmarshall.me>
2012-05-18 17:15:37 +1000

    Patching in support for das FLASH in the LEDButtonElement

--- LEDButtonElement.py
+++ LEDButtonElement.py
@@ -15,7 +15,7 @@ class LEDButtonElement(FlashingButtonElement):
       off_color = 0,
       on_color = 127,
       blink_on = False,
-      blink_colors = [],
+      blink_colors = [RED],
       color_mappings = None,
       **kwargs):
 
@@ -31,13 +31,15 @@ class LEDButtonElement(FlashingButtonElement):
     if color_mappings is not None:
       for value, note_offset in color_mappings.items():
         adjusted_identifier = self._msg_identifier + note_offset
-        self.leds[value] = ButtonElement(True, self._msg_type, self._msg_channel, adjusted_identifier)
+        if note_offset > 0: # Don't create an alias for the root button if it has one
+          self.leds[value] = ButtonElement(True, self._msg_type, self._msg_channel, adjusted_identifier)
 
   def send_value(self, value, force_send=False):
     if value == 0:
       FlashingButtonElement.send_value(self, value, force_send)
     else:
       if self.leds.has_key(value):
+        FlashingButtonElement.send_value(self, value, force_send)
         self.leds[value].send_value(127, True)
       else:
         FlashingButtonElement.send_value(self, value, force_send)
